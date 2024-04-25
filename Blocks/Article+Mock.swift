067dd145946fee8b80ed2cc7af3555eb094b761f
Tim Mitra <tim@it-guy.com>
2024-04-24 22:27:36 -0400

    Created Article model and mock instances

---
+++ Blocks/Article+Mock.swift
@@ -0,0 +1,27 @@
+//
+//  Article+Mock.swift
+//  Blocks
+//
+//  Created by Tim Mitra on 2024-04-24.
+//
+
+import Foundation
+
+extension Article {
+  
+  static let blockChain: Self = .init(
+    title: "Lego Block Chain",
+    date: Date(),
+    detail: "Add your own Lego block to the chain of transactions to guarantee security by trusting a whole crowd of people you've never met. What can possibly go wrong?",
+    smallImageName: "blockCircle",
+    largeImageName: "chain"
+  )
+  
+  static let airBlock: Self = .init(
+    title: "Air Block & Block",
+    date: Date(),
+    detail: "Rent out your Lego house for extra dollars. Create an extra revenue stream for your family by renting out your unused rooms.",
+    smallImageName: "houseSimple",
+    largeImageName: "houseInterior"
+  )
+}
---
+++ Blocks/Article.swift
@@ -0,0 +1,16 @@
+//
+//  Article.swift
+//  Blocks
+//
+//  Created by Tim Mitra on 2024-04-24.
+//
+
+import Foundation
+
+struct Article {
+  let title: String
+  let date: Date
+  let detail: String
+  let smallImageName: String
+  let largeImageName: String
+}
