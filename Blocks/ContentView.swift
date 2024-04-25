6c5bc9a908cbde93afb9d7d0cd5c3a504f68b258
Tim Mitra <tim@it-guy.com>
2024-04-24 22:10:09 -0400

    NewsCell: HStack and images

--- Blocks/ContentView.swift
+++ Blocks/ContentView.swift
@@ -47,12 +47,26 @@ struct ContentView_Previews: PreviewProvider {
 
 struct NewsCell: View {
   var body: some View {
-    VStack(alignment: .leading) {
-      Text("Text")
-        .font(.title2)
-      .fontWeight(.bold)
-      Text("Tertiary Text")
-        .foregroundColor(Color.gray)
+    VStack {
+      HStack {
+        Image(systemName: "photo")
+          .resizable(resizingMode: .stretch)
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 44.0, height: 44.0)
+        VStack(alignment: .leading) {
+          Text("Text")
+            .font(.title2)
+          .fontWeight(.bold)
+          Text("Tertiary Text")
+            .foregroundColor(Color.gray)
+        }
+      }
+      Text("Detail Text")
+      Image(systemName: "photo")
+        .resizable(resizingMode: .stretch)
+        .aspectRatio(contentMode: .fit)
+        .frame(maxHeight: 240.0)
+        .clipped()
     }
   }
 }
