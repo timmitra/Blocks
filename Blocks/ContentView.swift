77f001bab13542c0368e942b5e28753fc3e02e8a
Tim Mitra <tim@it-guy.com>
2024-04-24 21:33:24 -0400

    Added TabView with three tabItems

--- Blocks/ContentView.swift
+++ Blocks/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+      TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+        Text("Tab Content 1").tabItem { Text("News") }.tag(1)
+        Text("Tab Content 2").tabItem { Text("Products") }.tag(2)
+        Text("Tab Content 2").tabItem { Text("Chat") }.tag(2)
+      }
     }
 }
 
