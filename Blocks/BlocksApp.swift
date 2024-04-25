993ca14a9b4cc6c1f2ada5e8cf1cb519dcda34b3
Tim Mitra <tim@it-guy.com>
2024-04-24 21:27:15 -0400

    Initial Commit

---
+++ Blocks/BlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksApp.swift
+//  Blocks
+//
+//  Created by Tim Mitra on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct BlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blocks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blocks
+//
+//  Created by Tim Mitra on 2024-04-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
